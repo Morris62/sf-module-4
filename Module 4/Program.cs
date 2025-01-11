59593fd4005a1785a2baccecd7dd9ab98e07ba11
a.chaturov <chaturovai@gmail.com>
2025-01-11 23:43:09 +0300

    unit 4.1.18

--- Module 4/Program.cs
+++ Module 4/Program.cs
@@ -6,28 +6,31 @@ class Program
     {
         Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-        var color = Console.ReadLine();
-
-        if (color == "red")
+        switch (Console.ReadLine())
         {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Black;
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is red!");
+                break;
             
-            Console.WriteLine("Your color is red!");
-        }
-        else if (color == "green")
-        {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Black;
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is green!");
+                break;
             
-            Console.WriteLine("Your color is green!");
-        }
-        else
-        {
-            Console.BackgroundColor = ConsoleColor.Cyan;
-            Console.ForegroundColor = ConsoleColor.Black;
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is cyan!");
+                break;
             
-            Console.WriteLine("Your color is cyan!");
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Your color is yellow!");
+                break;
         }
     }
 }
