356b5d3c353de7db3b5d2ae735b545f0aa3591df
a.chaturov <chaturovai@gmail.com>
2025-01-15 00:45:46 +0300

    unit 4.3.12

--- Module 4/Program.cs
+++ Module 4/Program.cs
@@ -4,20 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[,] array = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        Console.WriteLine($"Количество строк: {array.GetUpperBound(0)+1}");
-        Console.WriteLine($"Количество колонок: {array.GetUpperBound(1)+1}");
-        
-        for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+        var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+
+        for (int i = 0; i < arr.Length; i++)
         {
-            for (int j = 0; j < array.GetUpperBound(1) + 1; j++)
-                Console.Write($"{array[j, i]} ");
-            Console.WriteLine();
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i] > arr[j])
+                {
+                    var k = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = k;
+                }
+            }
         }
 
-        foreach (var item in array)
+        foreach (var item in arr)
         {
-            Console.Write($"{item} ");
+            Console.Write(item + " ");
         }
+        
     }
 }
