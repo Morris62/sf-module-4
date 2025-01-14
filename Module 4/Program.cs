1d3164054649551cb84fc7012ba17e106be9e088
a.chaturov <chaturovai@gmail.com>
2025-01-15 00:55:28 +0300

    unit 4.3.13

--- Module 4/Program.cs
+++ Module 4/Program.cs
@@ -4,25 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+        var arr = new[] { 5, 6, 9, 1, 2, 3, 4 };
 
-        for (int i = 0; i < arr.Length; i++)
-        {
-            for (int j = i + 1; j < arr.Length; j++)
-            {
-                if (arr[i] > arr[j])
-                {
-                    var k = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = k;
-                }
-            }
-        }
-
+        int sum = 0;
+        
         foreach (var item in arr)
         {
-            Console.Write(item + " ");
+            sum += item;
         }
-        
+        Console.WriteLine("Сумма = " + sum);
     }
 }
