7e21bd76f96e7418e03a4be0344c9c5a175dbcc3
a.chaturov <chaturovai@gmail.com>
2025-01-15 23:55:43 +0300

    unit 4.4.3

--- Module 4/Program.cs
+++ Module 4/Program.cs
@@ -4,14 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        (string name, int age) anketa = new();
+        var (name, age) = ("Евгения", 27);
         
+        Console.WriteLine("Вас зовут {0}, Ваш возраст {1}", name, age.ToString());
+
         Console.WriteLine("Введите Ваше имя: "); 
-        anketa.name = Console.ReadLine();
+        name = Console.ReadLine();
         Console.WriteLine("Введите Ваш возраст: ");
-        anketa.age = int.TryParse(Console.ReadLine(), out int age) ? age : 0;
+        age = int.TryParse(Console.ReadLine(), out int _age) ? _age : 0;
         
-        Console.WriteLine("Вас зовут {0}, Ваш возраст {1}", anketa.name, anketa.age.ToString());
+        Console.WriteLine("Вас зовут {0}, Ваш возраст {1}", name, age.ToString());
         Console.ReadKey();
     }
 }
