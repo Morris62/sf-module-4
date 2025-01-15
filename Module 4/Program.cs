a83651bfc909497531116ba8ad2c0547a9d90979
a.chaturov <chaturovai@gmail.com>
2025-01-16 00:06:21 +0300

    unit 4.4.5

--- Module 4/Program.cs
+++ Module 4/Program.cs
@@ -4,16 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        var (name, age) = ("Евгения", 27);
+        (string Name, string Type, double Age, int NameCount) Pet;
         
-        Console.WriteLine("Вас зовут {0}, Ваш возраст {1}", name, age.ToString());
-
-        Console.WriteLine("Введите Ваше имя: "); 
-        name = Console.ReadLine();
-        Console.WriteLine("Введите Ваш возраст: ");
-        age = int.TryParse(Console.ReadLine(), out int _age) ? _age : 0;
+        Console.WriteLine("Введите имя питомца: "); 
+        Pet.Name = Console.ReadLine();
+        Pet.NameCount = Pet.Name.Length;
+        Console.WriteLine("Введите тип питомца: ");
+        Pet.Type = Console.ReadLine();
+        Console.WriteLine("Введите возраст питомца: ");
+        Pet.Age = double.TryParse(Console.ReadLine(), out var age) ? age : 0;
         
-        Console.WriteLine("Вас зовут {0}, Ваш возраст {1}", name, age.ToString());
+        Console.WriteLine("Имя питомца {0}, тип питомца {1}, возраст питомца {2}", Pet.Name, Pet.Type, Pet.Age.ToString("F1"));
         Console.ReadKey();
     }
 }
